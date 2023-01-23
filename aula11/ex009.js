1eb55bd2c1e235620bc27ab202bdc710689303a1
mateussdev <mateusbs427@gmail.com>
2023-01-23 15:17:28 -0300

    starting the revision

--- aula11/ex009.js
+++
@@ -1,6 +0,0 @@
-var pais = 'Itália'
-if(pais == 'Brasil'){ //condição composta
-    console.log('Brasileiro')
-}else{
-    console.log('Estrangeiro')
-}
--- aula14/ambiente.js
+++
@@ -1,5 +0,0 @@
-console.log("VAI COMEÇAR!")
-for(var c = 1; c <= 5; c++){
-    console.log(c)
-}
-console.log("FIM!")
